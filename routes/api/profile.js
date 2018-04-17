f8cd8850875de21b7c571e62f2eac671919d771d
Devin Riegle <riegledevin@gmail.com>
2018-04-16 22:01:01 -0400

    Added route for creating/updating profiles

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -60,6 +60,32 @@ router.post(
     if (req.body.facebook) profileFields.social.facebook = req.body.facebook;
     if (req.body.linkedin) profileFields.social.linkedin = req.body.linkedin;
     if (req.body.instagram) profileFields.social.instagram = req.body.instagram;
+
+    Profile.findOne({ user: req.user.id }).then(profile => {
+      if (profile) {
+        // Update
+        Profile.findOneAndUpdate(
+          { user: req.user.id },
+          { $set: profileFields },
+          { new: true }
+        ).then(profile => res.json(profile));
+      } else {
+        // Create
+        // Check if handle exists
+        Profile.findOne({ handle: profileFields.handle }).then(profile => {
+          if (profile) {
+            // Handle exists already, throw error
+            errors.handle = "That handle already exists";
+            return res.status(400).json(errors);
+          } else {
+            // Handle doesn't exist, Save Profile
+            new Profile(profileFields)
+              .save()
+              .then(profile => res.json(profile));
+          }
+        });
+      }
+    });
   }
 );
 
